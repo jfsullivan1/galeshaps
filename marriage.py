3d65d51b4b48cf5b03ae90fc60250db47057c90b
John <38336418+jfsullivan1@users.noreply.github.com>
2020-02-06 13:08:25 -0500

    finished code
    d(^_^ d)

--- marriage.py
+++ marriage.py
@@ -4,80 +4,101 @@
 # 1/30/2020
 
 import sys
+import time
 
-def tests():
+stopwatch = time.time()
+
+# =================================================================================
+# INVARIANTS: 
+# - Any person will always have n preferences for marriage partners. (The given n)
+# - There will always be n * 2 total people. 
+# - If a lady is removed from one of the knights preferences list, it means she preferred someone else over him. 
+# - Any engaged lady finds her current partner more desirable than any previous partners. 
+# =================================================================================
+
+def ReadFile():
+
+    # Setup
     args = sys.argv
-    dataFile = open(args[1], "r")
-    prefs = []
-    knights = []
-    ladies = []
-    line = dataFile.readline()
-    n = int(line)
-    totalPeople = n * 2
-    line = dataFile.readline()
-    while line:
-        prefs.append(line.split())
-        line = dataFile.readline()
-    i = 0
-    while i < totalPeople/2:
-        knights.append(prefs[i])
-        knights[i].append(0)
-        i+=1
 
-    j = 0
-    i = int(totalPeople/2)
-    while i < totalPeople:
-        ladies.append(prefs[i])
-        ladies[j].append(0)
-        i+=1
-        j+=1
+    # Try to open file 
+    try:
+        dataFile = open(args[1], "r")
+    except(IndexError):
+        exit(1)
+
+    marriedCouples = {}
+    singleKnights = []
+    knights = {}
+    ladies = {}
+    line = dataFile.readline()
+
+    # Get the 'n' amount of people from the first line in the file 
+    try:
+        n = int(line)
+    except(ValueError):
+        exit(1)
+
+    # Fill up the knights and ladies dictionaries. 
+    # Dictionary will contain a name as the key followed by their preferences as the value.
+    # Example,    Henry : [Mary, Cindy, Barbara]
+    for index, line in enumerate(dataFile):
+
+        # First, we are going to make sure the file is formatted correctly. 
+        # This simply checks the length of the preferences to see if it's correct length.  
+        if len(line.split()[1:]) > n or len(line.split()[1:]) < n:
+            exit(1)
+
+        # Here, everything is sorted into dictionaries. 
+        if(index < n):
+            knights [line.split()[0]] = line.split()[1:]
+            singleKnights.append(line.split()[0])
+        else:
+            ladies [line.split()[0]] = line.split()[1:]
+
+    marriedCouples = Matchmaking(knights, ladies, singleKnights, marriedCouples)
     
-    print("knights")
-    print(knights)
-    print("ladies")
-    print(ladies)
+    return marriedCouples
 
+def Matchmaking(knights, ladies, singleKnights, marriedCouples):
+    # =======================================================================
+    # Invariants: 
+    # - At the end of every loop, each lady is either married or has not been proposed to by a knight. 
+    # - At the end of every loop, our set of married couples is a matching. 
+    # =======================================================================
+    while singleKnights:
+        knight = singleKnights.pop(0)
+        lady = knights[knight].pop(0)
 
+        if(marriedCouples.get(lady)):
+            
+            # Checks if the position of current husband based on preference is less than proposee position 
+            #   if it is, marry him instead and make current husband single, 
+            #   else, she says no to the knight. 
+            if(ladies[lady].index(knight) < ladies[lady].index(marriedCouples.get(lady))):
+                newlySingleKnight = marriedCouples.get(lady)
+                marriedCouples[lady] = knight
+                singleKnights.append(newlySingleKnight)
+            
+            else:
+                singleKnights.append(knight)
 
-def Marriage():
-    args = sys.argv
-    dataFile = open(args[1], "r")
-    prefs = []
-    knights = []
-    ladies = []
-    line = dataFile.readline()
-    n = int(line)
-    totalPeople = n * 2
-    line = dataFile.readline()
-    while line:
-        prefs.append(line.split())
-        line = dataFile.readline()
-    i = 0
-    while i < totalPeople/2:
-        knights.append(prefs[i])
-        knights[i].append(0)
-        i+=1
+        # If both were unmarried, marry them. 
+        else:
+            marriedCouples[lady] = knight
 
-    j = 0
-    i = int(totalPeople/2)
-    while i < totalPeople:
-        ladies.append(prefs[i])
-        ladies[j].append(0)
-        i+=1
-        j+=1
-    
-    print("knights")
-    print(knights)
-    print("ladies")
-    print(ladies)
+    return marriedCouples
 
-#IMPORTANT
-#index n+1 will be the flag
-
+# Driver code
 def main():
-    Marriage()
+    
+    marriedList = ReadFile()
+    for couple in marriedList:
+        print(marriedList[couple], couple)
+    sys.stderr.write(" ========== Benchmark Time: %s sec. ==========" %(time.time() - stopwatch))
 
-main()
+if __name__ == "__main__":
+    main()
 
 
-#hey whazsxdcfgtyhujiokplkjhgfvcxcfvghjikolpl,m sawesrt6y78u90-=]\[p0o98]
+
